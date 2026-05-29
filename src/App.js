ea270aca65178565c2d4affa45a3987687a3ba01
Mayank123098 <Mayank@bipolarfactory.com>
2026-05-29 22:55:55 +0530

    Add CSV file upload functionality using PapaParse

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-
+import Papa from 'papaparse';
+import { useState,useRef } from 'react';
 
 function App() {
+  const [data , setData] = useState([]);
+   const fileInputRef = useRef(null);
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if(!file) return;
+    if(file.type !== 'text/csv' || !file.name.endsWith('.csv')) {
+      alert('Please upload a valid CSV file.');
+      return;
+    }
+
+    Papa.parse(file, {
+      header: true,
+      skipEmptyLines: true,
+      complete: (results) => {
+        setData(results.data);
+      }
+    });
+
+  }
+
+   const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  console.log(data);
+
   return (
     <div className="App">
-      Hello
+      <input type="file"
+        accept=".csv"
+        ref={fileInputRef}
+        onChange={handleFileUpload}
+        style={{ display: "none" }} />
+
+        <button onClick={handleButtonClick}>
+        Upload
+      </button>
+
     </div>
   );
 }
